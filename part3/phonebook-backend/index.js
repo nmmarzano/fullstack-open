be35d76c66c8b61ddc0209b35077a66a7f57429c
Stella <nmmarzano@gmail.com>
2021-02-28 13:31:00 -0300

    exercises 3.9-3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => {
     if (req.method === 'POST') {
@@ -102,12 +105,37 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.put('/api/persons/:id', (request, response) => {
+    if (!request.body) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    if (!request.body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!request.body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    const edited = {
+        id: request.params.id,
+        name: request.body.name,
+        number: request.body.number
+    }
+    persons = persons.map(person => person.id === edited.id? edited : person)
+    response.status(200).json(edited)
+})
+
 const unknownEndpoint = (request, response, next) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
